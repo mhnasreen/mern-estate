c4223bf8f21d53bfa73c6d56a74006fdc54b926c
github_username <mhnasreen979@gmail.com>
2023-11-14 20:15:40 +0530

    complete update user functionality

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 import { env } from 'node:process';
 
 export const verifyToken = (req, res, next) => {
-    const token = req.cookie.access_token;
+    const token = req.cookies.access_token;
 
     if(!token) return next(errorHandler(401, 'Unauthorized'));
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
